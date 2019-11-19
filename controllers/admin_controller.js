962a4ad7a14cc8612febe7eb385581fae51fa619
crisarevalom <00209718@uca.edu.sv>
2019-11-19 15:00:41 -0600

    navbar fixed

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,11 +1,11 @@
 async function laboratorios(req, res) {
-  res.render("labo_page_admin", { title: "Laboratorios", msg: "" });
+  res.render("labo_page_admin", { usad: req.session.admin, title: "Laboratorios", msg: "" });
 }
 
 module.exports.laboratorios = laboratorios;
 
 async function secondsUsers(req, res) {
-  res.render("users_page_admin", { title: "Mod_Usuarios", msg: "" });
+  res.render("users_page_admin", { usad: req.session.admin, title: "Mod_Usuarios", msg: "" });
 }
 
 module.exports.secondsUsers = secondsUsers;
