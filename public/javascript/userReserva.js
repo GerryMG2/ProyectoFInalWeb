a0f7be8e87853406ec99afba03ebd967208e96e8
GerryMG <gerardo.andresz4@live.com>
2019-11-19 03:49:21 -0600

    new timepicker and form reserva labs

---
+++ public/javascript/userReserva.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+start = () => {
+    var 
+
+
+    console.log("empezo");
+    $('.form_datetime').datetimepicker({
+        //language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+        });
+        
+};
+
+window.onload = start;
